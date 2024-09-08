915fd6b19b404fde77bd1967e65c4ca934699aa2
Giovana Salgado Ramos <130854605+GiovanaSalgado53@users.noreply.github.com>
2024-09-07 21:55:48 -0300

    Update dos novos dados.js

--- dados.js
+++ dados.js
@@ -1,77 +1,50 @@
 let personagens = [
+  {
+    nome: "Sonic the Hedgehog",
+    fraseInicial: "Ei, ei! Quer ser um herói como eu? Comece separando o lixo!",
+    descricao: "Reciclar é mais do que jogar o lixo na lixeira certa. É cuidar do nosso planeta, economizar recursos e garantir um futuro melhor para todos. Se o Sonic faz isso, por que você não faria também? Cada pequena ação, como separar o lixo, faz uma grande diferença. Juntos, podemos criar um mundo mais limpo e saudável para as próximas gerações.",
+    dicas: "Dicas rápidas do Sonic para separar o lixo: <br> Papel: Lixeira azul! (jornais, revistas, caixas) <br> Plástico: Lixeira amarela! (garrafas PET, embalagens) <br> Vidro: Lixeira verde! (garrafas, potes)<br> Metal: Lixeira vermelha! (latas, embalagens)",
+    imagem: "https://th.bing.com/th/id/R.ccec648e6131b60694d26c6b9b644903?rik=kpJPoZL42kQWeQ&pid=ImgRaw&r=0",
+    maisInformacoes: "https://www.gov.br/mma/pt-br/noticias/como-e-porque-separar-o-lixo",
+    tags: "protagonista rápido aventureiro heroico Separação do lixo coleta seletiva reciclagem sustentabilidade meio ambiente"
+  },
     {
       nome: "Shadow the Hedgehog",
-      primeiroJogo: ["Sonic Adventure 2",  2001 ],
-      jogos: ["Sonic Adventure 2", "Shadow the Hedgehog", "Sonic Heroes", "Sonic Rivals", "Sonic and the Black Knight",
-              "Sonic the Hedgehog (2006)", "Sonic Rivals 2", "Mario & Sonic at the Olympic Games", "Sonic Free Riders",
-              "Sonic Colors", "Sonic Generations", "Sonic Lost World", "Sonic Boom: Rise of Lyric", "Sonic Forces",
-              "Sonic Mania Plus", "Team Sonic Racing"],
-      descricao: "Criado como a 'forma de vida suprema', Shadow é um ouriço com habilidades sobre-humanas, conhecido por sua aparência sombria e seus poderes extraordinários.",
-      habilidades: ["Chaos Control", "Chaos Spear", "Chaos Dash", "Super Speed"],
+      fraseInicial: "A natureza é a força mais poderosa que conheço. Respeite-a.",
+      descricao: "Shadow, um ouriço com uma força incrível, sabe que a natureza é a fonte de toda a vida. As florestas, os oceanos, os animais – todos fazem parte de um equilíbrio delicado. A beleza da natureza nos inspira e nos conecta com algo maior do que nós mesmos. Ao proteger o meio ambiente, estamos protegendo nosso futuro e o futuro de todas as criaturas.",
+      dicas: "Dicas do Shadow para apreciar a natureza: <br>  Faça um piquenique em um parque. <br>  Observe as estrelas em uma noite clara. <br>  Plante uma árvore. <br>  Ajude a limpar um rio ou praia.",
       imagem: "https://pngpix.com/images/high/shadow-the-hedgehog-pose-lnbxbases0i3c070.webp",
-      maisInformacoes: "https://pt.wikipedia.org/wiki/Shadow_the_Hedgehog", 
-      tags: "protagonista anti-herói poderoso misterioso"
+      maisInformacoes: "https://www.nationalgeographicbrasil.com/", 
+      tags: "ouriço forte natureza beleza meio ambiente proteção ambiental",
     },
     {
-      nome: "Miles 'Tails' Prower",
-      primeiroJogo: ["Sonic the Hedgehog 2",  1992 ],
-      jogos: ["Sonic the Hedgehog 2", "Sonic Adventure", "Sonic Heroes", "Sonic Unleashed", "Sonic Colors", "Sonic Generations", "Sonic Lost World", "Sonic Boom: Rise of Lyric", "Sonic Forces","Sonic Mania", "Sonic Mania Plus", "Team Sonic Racing", "Sonic Frontiers"],
-      descricao: "Melhor amigo de Sonic, Tails é um raposa com duas caudas, gênio mecânico e piloto habilidoso.",
-      habilidades: ["Mecânica Avançada", "Inteligência Superdotada", "Voo com as Caudas"],
+      nome: "Tails Miles Prower",
+      fraseInicial: "Com a minha inteligência, podemos otimizar o planeta! Comece economizando energia!",
+      descricao: "O Tails, com sua mente brilhante, sabe que cada watt conta! Economizar energia é como dar um upgrade no planeta. Vamos juntos criar um mundo mais eficiente e sustentável, um circuito de cada vez!",
+      dicas: "Dicas do Tails para economizar energia: <br> Desligue os aparelhos eletrônicos quando não estiver usando. <br>  Troque as lâmpadas incandescentes por LED. <br> Ajuste a temperatura do ar condicionado. <br>  Use pilhas recarregáveis.",
       imagem: "https://pre00.deviantart.net/7223/th/pre/i/2012/178/3/e/tails_by_mike9711-d55129r.png",
-      maisInformacoes: "https://pt.wikipedia.org/wiki/Miles_Tails_Prower",
-      tags: "aliado mecânico inteligente fofo"
+      maisInformacoes: "https://www.raizen.com.br/blog/consumo-consciente-energia",
+      tags: "raposa mecânico inventor inteligente economia de energia sustentabilidade meio ambiente futuro",
+
     },
     {
-      nome: "Amy Rose",
-      primeiroJogo: ["Sonic CD", 1993 ],
-      jogos: ["Sonic CD", "Sonic Adventure", "Sonic Heroes", "Sonic Unleashed", "Sonic Colors", "Sonic Generations", "Sonic Lost World", "Sonic Boom: Rise of Lyric", "Sonic Forces","Sonic Mania", "Sonic Mania Plus", "Team Sonic Racing", "Sonic Frontiers"],
-      descricao: "Uma ouriça rosa apaixonada e determinada, fã de Sonic e conhecida por seu martelo gigante.",
-      habilidades: ["Martelo Gigante", "Ataque Spin", "Super Força"],
+      nome: "Amy Rose", 
+      fraseInicial: "Juntos, podemos cuidar da natureza! Seja gentil com os animais e as plantas!", 
+      descricao: "A Amy Rose é apaixonada por todos os seres vivos! Ela sabe que os animais e as plantas precisam da nossa ajuda para viverem felizes e saudáveis. Vamos juntos cuidar da natureza e construir um mundo mais bonito e colorido!",
+      dicas: "Dicas da Amy para proteger a fauna e flora: <br>  Plante uma árvore ou uma flor. <br>  Não alimente animais silvestres. <br>  Economize água. <br>  Não jogue lixo no chão.",
       imagem: "https://vignette.wikia.nocookie.net/p__/images/d/de/Team_Sonic_Racing_Amy_no_car.png/revision/latest/scale-to-width-down/338?cb=20180804024503&path-prefix=protagonist",
-      maisInformacoes: "https://pt.wikipedia.org/wiki/wiki/Amy_Rose", 
-      tags: "protagonista romântica determinada fofa"
-    }, 
-    {
-      nome: "Sonic the Hedgehog",
-      primeiroJogo: ["Sonic the Hedgehog", 1991],
-      jogos: ["Sonic the Hedgehog", "Sonic the Hedgehog 2", "Sonic CD", "Sonic Adventure", "Sonic Heroes", "Sonic Unleashed", "Sonic Colors", "Sonic Generations", "Sonic Lost World", "Sonic Boom: Rise of Lyric", "Sonic Forces", "Sonic Mania", "Sonic Mania Plus", "Team Sonic Racing", "Sonic Frontiers"],
-      descricao: "Um ouriço azul rápido como o raio, conhecido por sua velocidade e amor por aventuras.",
-      habilidades: ["Super Velocidade", "Homing Attack", "Spin Dash"],
-      imagem: "https://pluspng.com/img-png/sonic-hd-png-hd-sonic-clipart-2636.png",
-      maisInformacoes: "https://pt.wikipedia.org/wiki/Sonic_the_Hedgehog",
-      tags: "protagonista rápido aventureiro heroico"
-  }, 
+      maisInformacoes: "https://institutobiomas.org/",
+      tags: "ouriço rosa fofa protetora animais plantas natureza meio ambiente fauna flora"
+    
+    },  
   {
     nome: "Knuckles the Echidna",
-    primeiroJogo: ["Sonic the Hedgehog 3", 1994],
-    jogos: ["Sonic the Hedgehog 3", "Sonic & Knuckles", "Sonic Adventure", "Sonic Heroes", "Sonic Unleashed", "Sonic Colors", "Sonic Generations", "Sonic Lost World", "Sonic Boom: Rise of Lyric", "Sonic Forces", "Sonic Mania", "Sonic Mania Plus", "Team Sonic Racing", "Sonic Frontiers"],
-    descricao: "Um equidna vermelho forte e leal, guardião da Master Emerald e conhecido por sua força e habilidades de luta.",
-    habilidades: ["Força sobre-humana", "Escalada", "Glide"],
-    imagem: "https://vignette.wikia.nocookie.net/sonic-runners-reloaded/images/6/66/KnucklesHD.png/revision/latest?cb=20190420205842",
-    maisInformacoes: "https://pt.wikipedia.org/wiki/Knuckles_the_Echidna",
-    tags: "aliado guardião forte honrado"
-  },
-  {
-    nome: "Rouge the Bat",
-    primeiroJogo: ["Sonic Adventure 2", 2001],
-    jogos: ["Sonic Adventure 2", "Sonic Heroes", "Sonic Unleashed", "Sonic Colors", "Sonic Generations", "Sonic Lost World", "Sonic Boom: Rise of Lyric", "Sonic Forces", "Sonic Mania Adventures"],
-    descricao: "Uma sensual e misteriosa morcego, conhecida por sua habilidade em roubar e por sua paixão por joias.",
-    habilidades: ["Acrobacia", "Voo", "Força sobre-humana"],
-    imagem: "https://th.bing.com/th/id/R.a86d44863ce99dddd7894adfe60b3462?rik=Py8OTuQ8J%2bXsfg&pid=ImgRaw&r=0", 
-    maisInformacoes: "https://pt.wikipedia.org/wiki/Rouge_the_Bat",
-    tags: "anti-heroína ladra misteriosa charmosa"
- }, 
- {
-  nome: "Dr. Ivo Robotnik (Eggman)",
-  primeiroJogo: ["Sonic the Hedgehog", 1991],
-  jogos: ["Sonic the Hedgehog", "Sonic the Hedgehog 2", "Sonic CD", "Sonic Adventure", "Sonic Heroes", "Sonic Unleashed", "Sonic Colors", "Sonic Generations", "Sonic Lost World", "Sonic Boom: Rise of Lyric", "Sonic Forces", "Sonic Mania", "Sonic Mania Plus", "Team Sonic Racing", "Sonic Frontiers", /* Adicione mais jogos aqui */],
-  descricao: "Um cientista maluco com ambições de conquistar o mundo e dominar os animais. Conhecido por suas máquinas e robôs poderosos.",
-  habilidades: ["Gênio científico", "Criação de robôs", "Planejamento estratégico"],
-  imagem: "https://th.bing.com/th/id/R.f824f19d8c66831ed47db95178f59411?rik=KvA04ILYF9Ys3g&riu=http%3a%2f%2fwww.pngmart.com%2ffiles%2f13%2fDoctor-Eggman-PNG-HD.png&ehk=Aa%2bkayp2p0pIY5Hpv6eKaj1C%2b5TnTzBEupgjjJd86jg%3d&risl=1&pid=ImgRaw&r=0", 
-  maisInformacoes: "https://pt.wikipedia.org/wiki/Doctor_Eggman",
-  tags: "vilão cientista robótica ambicioso"
-} 
+    fraseInicial: "Guardião da Master Emerald e da nossa história! Vamos preservar nossas raízes!",
+    descricao: "Knuckles, o guardião da Master Emerald, sabe da importância de preservar a cultura e as tradições do seu povo. A cultura é como um tesouro, que precisa ser cuidado e passado adiante para as próximas gerações. Vamos juntos valorizar nossas raízes e construir um futuro mais rico em conhecimento!",
+    dicas: "Dicas do Knuckles para preservar a cultura: <br>  Aprenda sobre a história da sua família. <br>  Participe de eventos culturais da sua comunidade. <br>  Valorize as tradições e costumes do seu povo. <br>  Compartilhe suas histórias e conhecimentos com outras pessoas.",
+    imagem: "https://vignette.wikia.nocookie.net/sonic-runners-reloaded/images/6/66/KnucklesHD.png/revision/latest?cb=20190420205842",maisInformacoes:"http://portal.iphan.gov.br/",
+    tags: "echidna forte guardião cultura tradições história patrimônio"
+  }
   ];
 
 
